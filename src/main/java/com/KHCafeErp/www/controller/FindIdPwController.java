d5569589e129530ade8f2654c8702013bee22199
bj-an <57123726+bj-an@users.noreply.github.com>
2019-12-30 01:24:22 +0900

    fg

--- src/main/java/com/KHCafeErp/www/controller/FindIdPwController.java
+++ src/main/java/com/KHCafeErp/www/controller/FindIdPwController.java
@@ -1,12 +1,17 @@
 package com.KHCafeErp.www.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.KHCafeErp.www.dto.Staff;
 import com.KHCafeErp.www.service.face.FindIdPwService;
@@ -17,11 +22,11 @@ public class FindIdPwController {
 
 	private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
-	@RequestMapping(value="/findinfo/test" ,method=RequestMethod.GET)
+	@RequestMapping(value="/findinfo/findinfo" ,method=RequestMethod.GET)
 	public void findId() {
 		//		logger.info("�뀒�뒪�듃");
 	}
-	@RequestMapping(value="/findinfo/test" ,method=RequestMethod.POST)
+	@RequestMapping(value="/findinfo/findinfo" ,method=RequestMethod.POST)
 	public String findIdResult(Staff staff,Model model) {
 		//		logger.info("�뀒�뒪�듃");
 
@@ -36,6 +41,18 @@ public class FindIdPwController {
 			return "/findinfo/findidfail";
 
 	}
-	
+//	// 비밀번호 찾기
+//	@RequestMapping(value = "/findinfo/findinfo", method = RequestMethod.GET)
+//	@ResponseBody
+//	public String passwordSearch(@RequestParam("userId")int staffNo,
+//			@RequestParam("userEmail")String email,
+//			HttpServletRequest request) {
+//
+//		MailSender.mailSendWithPassword(staffNo, email, request);
+//		
+//		return "/findinfo/findinfo";
+//	}
+
+
 
 }
--- src/main/java/com/KHCafeErp/www/service/impl/FindIdPwServiceImpl.java
+++ src/main/java/com/KHCafeErp/www/service/impl/FindIdPwServiceImpl.java
@@ -1,7 +1,16 @@
 package com.KHCafeErp.www.service.impl;
 
+import java.util.Random;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
+import javax.servlet.http.HttpServletRequest;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.stereotype.Service;
 
 import com.KHCafeErp.www.dao.face.FindIdPwDao;
@@ -14,8 +23,11 @@ public class FindIdPwServiceImpl implements FindIdPwService{
 	
 	@Autowired 
 				FindIdPwDao findidpwDao;
-	          
-	
+	private SqlSessionTemplate sqlSession;
+
+
+         
+		//아이디 찾는 메서드
 	@Override
 	public Staff findid(Staff staff) {
 		
@@ -25,5 +37,65 @@ public class FindIdPwServiceImpl implements FindIdPwService{
 		
 	}
 
+	// 이메일 난수 만드는 메서드
+		private String init() {
+			Random ran = new Random();
+			StringBuffer sb = new StringBuffer();
+			int num = 0;
 
+			do {
+				num = ran.nextInt(75) + 48;
+				if ((num >= 48 && num <= 57) || (num >= 65 && num <= 90) || (num >= 97 && num <= 122)) {
+					sb.append((char) num);
+				} else {
+					continue;
+				}
+
+			} while (sb.length() < size);
+			if (lowerCheck) {
+				return sb.toString().toLowerCase();
+			}
+			return sb.toString();
+		}
+
+		// 난수를 이용한 키 생성
+		private boolean lowerCheck;
+		private int size;
+
+		public String getKey(boolean lowerCheck, int size) {
+			this.lowerCheck = lowerCheck;
+			this.size = size;
+			return init();
+		}
+	
+
+
+	// 패스워드 찾기 이메일 발송
+		public void mailSendWithPassword(int staffNo, String email, HttpServletRequest request) {
+			// 비밀번호는 6자리로 보내고 데이터베이스 비밀번호를 바꿔준다
+			String key = getKey(false, 6);
+			findidpwDao = sqlSession.getMapper(FindIdPwDao.class);
+			
+			findidpwDao.get
+			MimeMessage mail = MailSender.createMimeMessage();
+			String htmlStr = "<h2>안녕하세요"
+					+ "<p> 임시 비밀번호를 발급해드렸습니다.</p>"
+					+ "<p>임시로 발급 드린 비밀번호는 <h2 style='color : blue'>'" + key 
+					+ "(혹시 잘못 전달된 메일이라면 이 이메일을 무시하셔도 됩니다)";
+			try {
+				mail.setSubject("[MS :p] 임시 비밀번호가 발급되었습니다", "utf-8");
+				mail.setText(htmlStr, "utf-8", "html");
+				mail.addRecipient(RecipientType.TO, new InternetAddress(user_email));
+				MailSender.send(mail);
+			} catch (MessagingException e) { 
+				e.printStackTrace();
+			}
+			// 비밀번호 암호화해주는 메서드
+		
+			findidpwDao.searchPassword(staffNo, email, key);
+			
+		}
+
+	
+	
 }
