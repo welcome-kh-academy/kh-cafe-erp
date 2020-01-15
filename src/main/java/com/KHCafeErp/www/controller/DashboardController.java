f5f37a730dc8a51e804bd5a8e7dd2eb591ff3176
jijiny <yoojin9325@naver.com>
2020-01-15 12:18:39 +0900

    대시보드 공지사항

--- src/main/java/com/KHCafeErp/www/controller/DashboardController.java
+++ src/main/java/com/KHCafeErp/www/controller/DashboardController.java
@@ -1,5 +1,7 @@
 package com.KHCafeErp.www.controller;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
@@ -7,8 +9,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.KHCafeErp.www.dto.NoticeBoard;
 import com.KHCafeErp.www.service.face.DashboardService;
 
 @Controller
@@ -19,9 +23,12 @@ public class DashboardController {
 	@Autowired DashboardService dashboardService; 
 	
 	@RequestMapping(value="/dashboard/index")
-	public void getPage(HttpSession session, HttpServletRequest req) {
+	public void getPage(HttpSession session, HttpServletRequest req, Model model) {
 			logger.info("대쉬보드 접속");
 			
+			List<NoticeBoard> noticeList = dashboardService.getNoticeList();
+			System.out.println(noticeList);
 			
+			model.addAttribute("notice", noticeList);
 	}
 }
--- src/main/java/com/KHCafeErp/www/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/KHCafeErp/www/service/impl/DashboardServiceImpl.java
@@ -1,14 +1,22 @@
 package com.KHCafeErp.www.service.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.KHCafeErp.www.dao.face.DashboardDao;
+import com.KHCafeErp.www.dto.NoticeBoard;
 import com.KHCafeErp.www.service.face.DashboardService;
 
 @Service
 public class DashboardServiceImpl implements DashboardService {
 
-	@Autowired DashboardDao dashboardDao; 
+	@Autowired DashboardDao dashboardDao;
+
+	@Override
+	public List<NoticeBoard> getNoticeList() {
+		return dashboardDao.getNoticeList();
+	} 
 	
 }
