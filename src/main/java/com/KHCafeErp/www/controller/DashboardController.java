345f35212220568a87fc673ca9abdaa36750b239
jijiny <yoojin9325@naver.com>
2020-01-15 11:53:29 +0900

    autowired

--- src/main/java/com/KHCafeErp/www/controller/DashboardController.java
+++ src/main/java/com/KHCafeErp/www/controller/DashboardController.java
@@ -5,13 +5,18 @@ import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.KHCafeErp.www.service.face.DashboardService;
 
 @Controller
 public class DashboardController {
 
 	private static Logger logger = LoggerFactory.getLogger(DashboardController.class);
+	
+	@Autowired DashboardService dashboardService; 
 	
 	@RequestMapping(value="/dashboard/index")
 	public void getPage(HttpSession session, HttpServletRequest req) {
--- src/main/java/com/KHCafeErp/www/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/KHCafeErp/www/service/impl/DashboardServiceImpl.java
@@ -1,10 +1,14 @@
 package com.KHCafeErp.www.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.KHCafeErp.www.dao.face.DashboardDao;
 import com.KHCafeErp.www.service.face.DashboardService;
 
 @Service
 public class DashboardServiceImpl implements DashboardService {
 
+	@Autowired DashboardDao dashboardDao; 
+	
 }
